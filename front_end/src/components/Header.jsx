a0fb9dc0448da072c65effcb4f2b67b20626e085
Tanju <terdinch123@gmail.com>
2023-05-03 23:03:49 +0300

    ReactTasim

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../components/Header.css'
+import { BsSearch } from "@react-icons/all-files/bs/BsSearch"
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='Header'>
+      <nav className='container'>
+         <h1>LOGO</h1>
+        <ul className='flex'>
+          <li>НАЧАЛО</li>
+          <li>ОНЛАЙН ПАКЕТИ</li>
+          <li>КОЙ Е ТАНЖУ ?</li>
+          <li>КОНТАКТИ</li>
+        </ul>
+        <div className='flex'>
+           <div className="profile_icon">
+              <i class="fa-regular fa-user"></i>
+              <span>Вход</span>
+            </div>
+            <div className="search_icon">
+              <BsSearch/>
+            </div>
+        </div>
+      </nav>
+       
     </div>
   )
 }
