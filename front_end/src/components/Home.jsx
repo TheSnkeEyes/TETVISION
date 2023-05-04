6326135585cbc6265465f4bd6383857b2dc8f011
Tanju <terdinch123@gmail.com>
2023-05-04 17:31:37 +0300

    Tanju_17:31

---
+++ front_end/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../components/Home.css'
+
+function Home() {
+  return (
+    <div>
+      <div className="Home">
+        <div className='motivasion'>
+            <h1>БЪДИ КОНГ СЪС ТАНЖУ</h1>
+            <h3>Нека заедно напишем вашата история за промяна !</h3>
+            <button>ЗА ПОДКРЕПА</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
