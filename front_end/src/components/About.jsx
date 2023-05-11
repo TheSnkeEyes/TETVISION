a38bd54ef75cf5b6897ebbda09187352bd9f7cc4
Tanju <terdinch123@gmail.com>
2023-05-12 00:49:52 +0300

    0:49

--- front_end/src/components/About.jsx
+++ front_end/src/components/About.jsx
@@ -67,15 +67,17 @@ function About() {
         <div className="training_tempalte">
           <div className="programs training_program">
             
+            <div className="layout"></div>
+            <div className="showing_line"><span>ТРЕНИРОВАЧНА ПРОГРАМА</span></div>
           </div>
           <div className="programs training_program">
-
+            <div className="layout"></div>
           </div>
           <div className="programs training_program">
-
+            <div className="layout"></div>
           </div>
           <div className="programs training_program">
-
+            <div className="layout"></div>
           </div>
 
         </div>
