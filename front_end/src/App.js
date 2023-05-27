50afe69ea837dba23bb8cb306a6ae2fbac9beae5
TheSnkeEyes <terdinch123@gmail.com>
2023-05-27 22:20:37 +0300

    22:20

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,18 +3,20 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Packages from "./components/Packages";
 import Transforms from "./components/Transforms";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <About/>
-      <Contact/>
-      <Transforms/>
-      <Footer/>
+      <Header />
+      <Home />
+      <About />
+      <Contact />
+      <Transforms />
+      <Packages />
+      <Footer />
     </div>
   );
 }
---
+++ front_end/src/components/Packages.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../components/Packages.css'
+
+function Packages() {
+    return (
+        <>
+            <div className="packages_section">
+                <div className="packages_title_section">
+                    <h4>ОНЛАЙН ПАКЕТИ</h4>
+                    <h1>Сравнителна таблица</h1>
+                </div>
+                <div className="packages_grid_templates">
+                    <div className="first_grid packages_grid_section">
+                        <h1 className='package-title'>1 Месец</h1>
+                       
+                    </div>
+                    <div className="second_grid packages_grid_section">
+
+                    </div>
+                    <div className="third_grid packages_grid_section">
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Packages
